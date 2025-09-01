ee067d6734cc0af579a465f4b52d792d37e6d86a
shafayatGit <shafayathossain.ielts@gmail.com>
2025-09-01 10:03:21 +0600

    testimonial section completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../Components/Hero";
 import FeaturedDestination from "../../Components/FeaturedDestination";
 import ExclusiveOffer from "../../Components/ExclusiveOffer";
+import Testimonial from "../../Components/Testimonial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <ExclusiveOffer/>
+      <Testimonial/>
     </>
   );
 };
