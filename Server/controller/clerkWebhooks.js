31ae9dba6eabe895f243128ab106da6d5c933a03
shafayatGit <shafayathossain.ielts@gmail.com>
2025-09-20 18:40:37 +0600

    webhook added

---
+++ Server/controller/clerkWebhooks.js
@@ -0,0 +1,4 @@
+import User from "../models/user.js";
+import { Webhook } from "svix";
+
+
--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,16 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 const PORT = process.env.PORT || 3000;
+import { clerkMiddleware } from '@clerk/express'
 
 connectDB()
 const app = express();
 app.use(cors());
+
+// middleWares
+app.use(clerkMiddleware());
+app.use(express.json());
+
 
 app.get("/",(req,res)=>res.send("TripLodge Running"))
 app.listen(PORT, () => {
